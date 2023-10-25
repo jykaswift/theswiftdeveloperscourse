dce287898269fd2c69fd6d0fa1efd5e01d11499d
jykaswift <jykaswift@mail.ru>
2023-10-25 17:31:09 +0300

    Lesosn 15 done

---
+++ UIButton(15L)/TImer/TImer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TImer
+//
+//  Created by Евгений Борисов on 25.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
